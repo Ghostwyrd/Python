00411e6282333d3d67e800afc51b7238b3ef47e6
Ghostwyrd <ghostwyrd@gmail.com>
2023-03-30 12:29:50 -0700

    removed test.txt as unnecessary

---
+++ practice.py
@@ -0,0 +1,16 @@
+
+def strange_list_fun(n):
+    strange_list = []
+
+    for i in range(0,n):
+            strange_list.append(i)
+        
+    return strange_list
+
+print(strange_list_fun(5))
+
+def wishes():
+    print("My Wishes")
+    return "BROKEN"
+
+print(wishes())
